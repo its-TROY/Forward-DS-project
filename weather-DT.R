00a9ed05c304468a441aae95eaa9f2cafbfae83b
DiaaAhmed <diaaMujahed@gmail.com>
2023-05-19 14:48:15 +0300

    Reload

--- weather-DT.R
+++ weather-DT.R
@@ -1,2 +1,9 @@
+#setwd("D:\\Programing\\universty programs\\Rlang\\Forward-DS-project") Diaa
+#setwd("")Youssef
+#setwd("")Mohmed
+#setwd("")Mahmoud
+#getwd()
 weather_training<-read.csv("Weather Training Data.csv")
-
+#weather_training
+table(weather_training$WindGustDir)
+barplot(weather_training$WindGustDir,xlab = weather_training$Location, ylab = weather_training$Rainfall)
