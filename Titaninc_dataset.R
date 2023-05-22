2519e386594e4527c265d2ab6075259cdb818e9f
Rinzler <divosilver.144@gmail.com>
2023-05-23 00:53:26 +0300

    some visualization

--- Titaninc_dataset.R
+++ Titaninc_dataset.R
@@ -1,8 +1,8 @@
-setwd("D:\\Programing\\universty programs\\Rlang\\Forward-DS-project")
+#setwd("D:\\Programing\\universty programs\\Rlang\\Forward-DS-project")
+setwd("C:/Users/divos/Documents/R/forward-project")#youssef
 getwd()
 myData <- read.csv("Titanic.csv")
 
-view(myData)
 print(which(duplicated(myData)))
 is.na(myData)
 
@@ -15,8 +15,11 @@ clean_data <- myData[-which(myData$Age %in% age_outliers),]
 boxplot(clean_data$Age)
 
 
+hist(clean_data$Age, xlab = "passenger age")# age freq
 
 
+survival_females = count(clean_data[which(clean_data$Gender == "female" & clean_data$Survived == 1), ])
+survival_males = count(clean_data[which(clean_data$Gender == "male" & clean_data$Survived == 1), ])
 
 
-
+pie(c(survival_males, survival_females), main = 'survivals in terms of gender')
