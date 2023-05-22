3d138da00c7c83aba4676d178589baf296170b15
mohab <mohamedmohab6969@gmail.com>
2023-05-23 02:01:48 +0300

    decision tree

--- Titaninc_dataset.R
+++ Titaninc_dataset.R
@@ -1,7 +1,10 @@
-#setwd("D:\\Programing\\universty programs\\Rlang\\Forward-DS-project")
-setwd("C:/Users/divos/Documents/R/forward-project")#youssef
+#setwd("D:\\Programing\\universty programs\\Rlang\\Forward-DS-project")#diaa
+#setwd("C:/Users/divos/Documents/R/forward-project")#youssef
+setwd("C:\\Users\\moham\\OneDrive\\Desktop\\Forward-DS-project")#mohamed
 getwd()
 myData <- read.csv("Titanic.csv")
+library(rpart)
+library(rpart.plot)
 
 print(which(duplicated(myData)))
 is.na(myData)
@@ -13,15 +16,21 @@ age_outliers <- boxplot(myData$Age)$out
 
 clean_data <- myData[-which(myData$Age %in% age_outliers),]
 boxplot(clean_data$Age)
-        
+
 
 hist(clean_data$Age, xlab = "passenger age")# age freq
 
 
-survival_males = as.integer(count(clean_data[which(clean_data$Gender == "male" & clean_data$Survived == 1), ]))
-survival_females = as.integer(count(clean_data[which(clean_data$Gender == "female" & clean_data$Survived == 1), ]))
-sur_count = count(clean_data[clean_data$Survived == 1, ])
-percentage = c(paste0(round(100*survival_males / sur_count), "%"), paste0(round(100*survival_females / sur_count), "%"))
+survival_females = count(clean_data[which(clean_data$Gender == "female" & clean_data$Survived == 1), ])
+survival_males = count(clean_data[which(clean_data$Gender == "male" & clean_data$Survived == 1), ])
 
-pie(c(survival_males, survival_females), main = 'survivals in terms of gender', labels = percentage, col = c("lightblue", "pink"))
-legend("bottomright", legend = c("male", "female"), fill = c("lightblue", "pink"))
+
+pie(c(survival_males, survival_females), main = 'survivals in terms of gender')
+
+
+tree<-rpart(Survived ~Age+Pclass,data = clean_data,minsplit=2)
+tree
+rpart.plot(tree)
+
+data_to_predict<-data.frame(Age=31,Pclass=1)
+predict(tree,newdata =data_to_predict )
