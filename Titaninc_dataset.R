5cc452d16d32a5a506818af7057926de140b0501
Rinzler <divosilver.144@gmail.com>
2023-05-23 21:15:07 +0300

    more visualization

--- Titaninc_dataset.R
+++ Titaninc_dataset.R
@@ -14,14 +14,24 @@ age_outliers <- boxplot(myData$Age)$out
 clean_data <- myData[-which(myData$Age %in% age_outliers),]
 boxplot(clean_data$Age)
         
+#-----------histogram----------------
+hist(clean_data$Age, xlab = "passenger age", main = "")# age freq
 
-hist(clean_data$Age, xlab = "passenger age")# age freq
-
-
+#------------------pie chart----------------
 survival_males = as.integer(count(clean_data[which(clean_data$Gender == "male" & clean_data$Survived == 1), ]))
 survival_females = as.integer(count(clean_data[which(clean_data$Gender == "female" & clean_data$Survived == 1), ]))
 sur_count = count(clean_data[clean_data$Survived == 1, ])
 percentage = c(paste0(round(100*survival_males / sur_count), "%"), paste0(round(100*survival_females / sur_count), "%"))
 
-pie(c(survival_males, survival_females), main = 'survivals in terms of gender', labels = percentage, col = c("lightblue", "pink"))
-legend("bottomright", legend = c("male", "female"), fill = c("lightblue", "pink"))
+pie(c(survival_males, survival_females), main = 'survivals in terms of gender', labels = percentage, col = c("brown", "lightblue"))
+legend("bottomright", legend = c("male", "female"), fill = c("brown", "lightblue"))
+
+#-----------------bar plot------------------
+males_fares = clean_data[clean_data$Gender == "male", "Fare"]
+females_fares = clean_data[clean_data$Gender == "female", "Fare"]
+
+fares_avg = data.frame(
+  males = as.integer(sum(males_fares) / length(males_fares)),
+  females = as.integer(sum(females_fares) / length(females_fares))
+)
+barplot(height = c(fares_avg$males, fares_avg$females), names.arg = c("males", "females"), horiz = T)
